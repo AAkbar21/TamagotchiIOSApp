8d910c35e4b6cb39b28fd6b53b05f6ff0f1bbddd
Abdullah Akbar <Akbar.A@etoncollege.org.uk>
2023-01-14 10:52:41 +0000

    Initial Commit

---
+++ TamagotchiIOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TamagotchiIOSApp
+//
+//  Created by Akbar, Abdullah (RCH) on 14/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TamagotchiIOSApp/TamagotchiIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiIOSAppApp.swift
+//  TamagotchiIOSApp
+//
+//  Created by Akbar, Abdullah (RCH) on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
